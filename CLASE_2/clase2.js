51d2de4e822027281093392bc58e1e2db271a603
Sandra <sandra.albin@hotmail.com>
2022-03-21 22:28:05 -0300

    tp

---
+++ CLASE_2/clase2.js
@@ -0,0 +1,46 @@
+const personajes = [
+    {
+        id: 1,
+        nombre: 'IronMan',
+        tipo : 'superheroe',
+        edad: 45
+    },
+    {
+        id: 2,
+        nombre: 'Viuda Negra',
+        tipo : 'superheroe',
+        edad: 35
+    },
+    {
+        id: 3,
+        nombre: 'Capitan America',
+        tipo : 'superheroe',
+        edad: 100
+    },
+    {
+        id: 4,
+        nombre: 'Hulk',
+        tipo : 'superheroe',
+        edad: 37
+    },
+    {
+        id: 5,
+        nombre: 'Thor',
+        tipo : 'superheroe',
+        edad: 105
+    }
+];
+
+console.log(personajes);
+
+const mostrarPorConsola = () => {
+    let persId = document.getElementById("personajeId").value;
+    let personajeBuscado = personajes.filter(p => p.id.toString() === persId);
+    console.log(personajeBuscado);
+}
+
+console.log(personajeBuscado);
+
+const perFil = personajes.map(p => p.nombre);
+console.log(perFil);
+
