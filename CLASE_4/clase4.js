a63e51c7463ae375efdac2640102073c330eabfa
Sandra <sandra.albin@hotmail.com>
2022-04-24 12:30:53 -0300

    "agrego prev"

--- CLASE_4/clase4.js
+++ CLASE_4/clase4.js
@@ -1,12 +1,11 @@
+let Actual;
+
 const start = async () => {
     try{
     const response = await fetch('https://rickandmortyapi.com/api/character');
     const {info, results} = await response.json();
-    //console.log(results);
-    results.forEach((character) => {
-        const card = mostrarTarjetas (character);
-        document.getElementById('cards').insertAdjacentHTML('beforeend', card);
-    })
+    Actual = 'https://rickandmortyapi.com/api/character'
+    mostrarTarjetas(results);
 
     }
     catch(err){
@@ -14,36 +13,37 @@ const start = async () => {
     }
 }
 
-    const mostrarTarjetas = (character) =>{
+const mostrarTarjetas = (results) =>{
+    document.getElementById("cards").innerHTML = '';
+    results.forEach((character) => {
         const card = `
         <div class = "col">
         <div class="card h-100" >
         <img height="200px" src="${character.image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${character.name}</h5>
-          <p class="card-text">Origen : ${character.origin.name}.</p>
-          <p class="card-text">Estado : ${character.status}.</p>
-          <p class="card-text">Especie : ${character.species}.</p>
+            <h5 class="card-title">${character.name}</h5>
+            <p class="card-text">Origen : ${character.origin.name}.</p>
+            <p class="card-text">Estado : ${character.status}.</p>
+            <p class="card-text">Especie : ${character.species}.</p>
         </div>
         </div>
         </div>`;
-    return card;
 
-    };
-    const buscarPorNombre = async () => {
+        document.getElementById('cards').insertAdjacentHTML('beforeend', card);
+    })
+
+};
+
+const buscarPorNombre = async () => {
         
-        try{
-            const name = document.getElementById('name').value;
-            const response = await fetch(`https://rickandmortyapi.com/api/character?name=${name}`);
-            const {info, results} = await response.json();
-        
-            document.getElementById("cards").innerHTML = '';
-            results.forEach((character) => {
-            const card = mostrarTarjetas (character);
-            document.getElementById('cards').insertAdjacentHTML('beforeend', card);
-        })
-       
-        
+    try{
+        const name = document.getElementById('name').value;
+        const response = await fetch(`https://rickandmortyapi.com/api/character?name=${name}`);
+        const {info, results} = await response.json();
+
+        mostrarTarjetas(results);
+    
+     
     } catch(err){
         console.error(err);
     }
@@ -55,18 +55,13 @@ const buscarPorEstado = async () => {
         const status = select.options[select.selectedIndex].text;
         const response = await fetch(`https://rickandmortyapi.com/api/character?status=${status}`);
         const {info, results} = await response.json();
-        document.getElementById("cards").innerHTML = '';
-        results.forEach((character) => {
-             const card = mostrarTarjetas(character);
-             document.getElementById('cards').insertAdjacentHTML('beforeend', card);           
-        })
+
+        mostrarTarjetas(results);
+       
     } catch(err){
         console.error(err);
     }
 }
-
-   
-
 
 const buscarPorEspecie = async () => {
         
@@ -75,18 +70,52 @@ const buscarPorEspecie = async () => {
         console.log(species);
         const response = await fetch(`https://rickandmortyapi.com/api/character?species=${species}`);
         const {info, results} = await response.json();
-    
-        document.getElementById("cards").innerHTML = '';
-        results.forEach((character) => {
-        const card = mostrarTarjetas (character);
-        document.getElementById('cards').insertAdjacentHTML('beforeend', card);
-    })
-   
+        mostrarTarjetas(results);
     
 } catch(err){
     console.error(err);
 }
+
 }
 
+const next = async() => {
+    try{
+        const response = await fetch(Actual);
+        const {info} = await response.json();
+        if(info.next != null){
+        const resp = await fetch(info.next);
+        const {results} = await resp.json();
 
-    window.onload = start();
+        Actual = info.next;
+
+        mostrarTarjetas(results);
+
+    } else {
+        window.alert("No hay más páginas siguientes")
+    }    
+    } catch(err){
+        console.error(err);
+    }
+}
+
+const prev = async() => {
+    try{
+        const response = await fetch(Actual);
+        const {info} = await response.json();
+        if(info.prev != null){
+            const resp = await fetch(info.prev);
+            const {results} = await resp.json();
+        
+            Actual = info.prev;
+
+            mostrarTarjetas(results);
+        } else {
+            window.alert("No hay páginas previas")
+        }
+
+    } catch(err){
+        console.error(err);
+    }
+}
+
+window.onload = start();
